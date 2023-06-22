cc81a716074ff7edf7a4341acfb1a9c29c70c90c
24Syleo <sylvain.beggiora@outlook.com>
2023-06-22 17:59:51 +0200

    cors

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ import connectDb from './db/connect.js';
 import express from 'express';
 import beerRouter from './router/BeerRouter.js';
 import ingRouter from './router/IngredientRouter.js'
+import cors from './cors/cors.js';
 
 const port = process.env.PORT
 const app = express();
 
 connectDb().catch(err => console.log(err));
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/beer", beerRouter);
