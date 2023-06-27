3e46ac1b3134be14de0026ef9d88a8fee13dbbdb
24Syleo <sylvain.beggiora@outlook.com>
2023-06-27 11:35:26 +0200

    ok

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 import 'dotenv/config';
 import connectDb from './db/connect.js';
 import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
 import beerRouter from './router/BeerRouter.js';
 import ingRouter from './router/IngredientRouter.js'
 import cors from 'cors';
 
 const port = process.env.PORT
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 connectDb().catch(err => console.log(err));
 
@@ -16,5 +20,11 @@ app.use(express.json());
 app.use("/beer", beerRouter);
 app.use("/ingredient", ingRouter);
 
+io.on("connection", (socket) => {
+    console.log("connected");
+    socket.on("disconnect",()=>{
+        console.log("disconnected");
+    });
+});
 
-app.listen(port, () => console.log('listening on port: ' + port));
+server.listen(port, () => console.log('listening on port: ' + port));
